077ed82fae7653782ef78648492b43d1c9136741
chandle greff <unconfigured@null.spigotmc.org>
2022-04-18 19:37:58 +0200

    first commit

---
+++ src/main/java/io/rently/mailerservice/MailerserviceApplication.java
@@ -0,0 +1,13 @@
+package io.rently.mailerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailerserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/rently/mailerservice/MailerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rently.mailerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
