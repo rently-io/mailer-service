a9d64579d33d5154724be0ce8387c772ba1fa5eb
chandle greff <unconfigured@null.spigotmc.org>
2022-04-22 19:30:51 +0200

    Added get prop from map helper

---
+++ src/main/java/io/rently/mailerservice/utils/Properties.java
@@ -0,0 +1,20 @@
+package io.rently.mailerservice.utils;
+
+import io.rently.mailerservice.errors.Errors;
+
+import java.util.Map;
+
+public class Properties {
+
+    private Properties() {
+    }
+
+    public static String tryGetProperty(String property, Map<String, Object> data) {
+        try {
+            return data.get(property).toString();
+        } catch (Exception ignore) {
+            throw new Errors.HttpBodyFieldMissing(property);
+        }
+    }
+
+}
