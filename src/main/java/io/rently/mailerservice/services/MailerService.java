40f650a602eccd7b3c83435e18b92837e6163587
chandle greff <unconfigured@null.spigotmc.org>
2022-04-25 06:20:59 +0200

    Added default const

--- src/main/java/io/rently/mailerservice/services/MailerService.java
+++ src/main/java/io/rently/mailerservice/services/MailerService.java
@@ -5,18 +5,18 @@ import io.rently.mailerservice.interfaces.IMessenger;
 import io.rently.mailerservice.mailer.Mailer;
 import io.rently.mailerservice.mailer.templates.*;
 import io.rently.mailerservice.utils.Broadcaster;
-import io.rently.mailerservice.utils.Properties;
-import org.json.JSONObject;
+import io.rently.mailerservice.utils.Fields;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Map;
 
 @Service
 public class MailerService {
     private final IMessenger mailer;
 
+    @Autowired
     public MailerService(
             @Value("${mailer.password}") String password,
             @Value("${mailer.email}") String email,
@@ -25,11 +25,17 @@ public class MailerService {
         this.mailer = new Mailer.Builder(email).credentials(email, password).host(host).build();
     }
 
+    public MailerService(IMessenger mailer) {
+        this.mailer = mailer;
+    }
+
     public void sendNotification(Map<String, Object> data) {
-        String subject = Properties.tryGet("subject", data);
-        String body = Properties.tryGet("body", data);
-        String email = Properties.tryGet("email", data);
+        String subject = Fields.tryGet("subject", data);
+        String body = Fields.tryGet("body", data);
+        String email = Fields.tryGet("email", data);
+
         Broadcaster.info("Sending generic notification to " + email);
+
         try {
             mailer.sendEmail(email, subject, Notification.getTemplate(subject, body));
         } catch(Exception ex) {
@@ -38,9 +44,11 @@ public class MailerService {
     }
 
     public void sendGreetings(Map<String, Object> data) {
-        String name = Properties.tryGet("name", data);
-        String email = Properties.tryGet("email", data);
+        String name = Fields.tryGet("name", data);
+        String email = Fields.tryGet("email", data);
+
         Broadcaster.info("Sending greetings to " + email);
+
         try {
             mailer.sendEmail(email, "Nice to meet you, " + name, Welcome.getTemplate(name));
         } catch(Exception ex) {
@@ -49,15 +57,14 @@ public class MailerService {
     }
 
     public void sendNewListingNotification(Map<String, Object> data) {
-        String email = Properties.tryGet("email", data);
-        String link = Properties.tryGet("link", data);
-        String image = Properties.tryGet("image", data);
-        String title = Properties.tryGet("title", data);
-        String description = Properties.tryGet("description", data);
-        if (description.length() > 100) {
-            description = description.substring(0, 100).trim() + "...";
-        }
+        String email = Fields.tryGet("email", data);
+        String link = Fields.tryGet("link", data);
+        String image = Fields.tryGet("image", data);
+        String title = Fields.tryGet("title", data);
+        String description = Fields.tryGetElipsed("description", data, 100);
+
         Broadcaster.info("Sending new listing prompt to " + email);
+
         try {
             mailer.sendEmail(email, "Listing online!", NewListing.getTemplate(link, image, title, description));
         } catch(Exception ex) {
@@ -66,9 +73,11 @@ public class MailerService {
     }
 
     public void sendAccountDeletionNotification(Map<String, Object> data) {
-        String email = Properties.tryGet("email", data);
-        String name = Properties.tryGet("name", data);
+        String email = Fields.tryGet("email", data);
+        String name = Fields.tryGet("name", data);
+
         Broadcaster.info("Sending goodbyes to " + email);
+
         try {
             mailer.sendEmail(email, "Account remove from Rently", Goodbye.getTemplate(name));
         } catch(Exception ex) {
@@ -77,13 +86,12 @@ public class MailerService {
     }
 
     public void sendListingDeletionNotification(Map<String, Object> data) {
-        String email = Properties.tryGet("email", data);
-        String title = Properties.tryGet("title", data);
-        String description = Properties.tryGet("description", data);
-        if (description.length() > 100) {
-            description = description.substring(0, 100).trim() + "...";
-        }
+        String email = Fields.tryGet("email", data);
+        String title = Fields.tryGet("title", data);
+        String description = Fields.tryGetElipsed("description", data, 100);
+
         Broadcaster.info("Sending listing deletion prompt to " + email);
+
         try {
             mailer.sendEmail(email, "Listing removed", ListingDeletion.getTemplate(title, description));
         } catch(Exception ex) {
--- src/main/java/io/rently/mailerservice/services/ReporterService.java
+++ src/main/java/io/rently/mailerservice/services/ReporterService.java
@@ -4,17 +4,21 @@ import io.rently.mailerservice.interfaces.IMessenger;
 import io.rently.mailerservice.mailer.Mailer;
 import io.rently.mailerservice.mailer.templates.DevError;
 import io.rently.mailerservice.utils.Broadcaster;
-import io.rently.mailerservice.utils.Properties;
+import io.rently.mailerservice.utils.Fields;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+@Service
 public class ReporterService {
     private final List<String> firstResponders;
     private final IMessenger mailer;
 
+    @Autowired
     public ReporterService(
             @Value("#{'${first.responders}'.split(',')}") List<String> firstResponders,
             @Value("${mailer.password}") String password,
@@ -25,12 +29,17 @@ public class ReporterService {
         this.mailer = new Mailer.Builder(email).credentials(email, password).host(host).build();
     }
 
+    public ReporterService(List<String> firstResponders, IMessenger mailer) {
+        this.firstResponders = firstResponders;
+        this.mailer = mailer;
+    }
+
     public void sendReportToDevs(Map<String, Object> data) {
-        String service = Properties.tryGetOptional("service", data, "Unknown source");
-        String message = Properties.tryGetOptional("message", data, "No message");
-        String cause = Properties.tryGetOptional("cause", data, "Not specified");
-        String trace = Properties.tryGetOptional("trace", data, "No trace");
-        String exceptionType = Properties.tryGetOptional("exceptionType", data, "Unknown");
+        String service = Fields.tryGetOptional("service", data, "Unknown source");
+        String message = Fields.tryGetOptional("message", data, "No message");
+        String cause = Fields.tryGetOptional("cause", data, "Not specified");
+        String trace = Fields.tryGetOptional("trace", data, "No trace");
+        String exceptionType = Fields.tryGetOptional("exceptionType", data, "Unknown");
 
         Broadcaster.info("Dispatching error report from " + service + " to " + firstResponders.size() + " dev(s)");
 
