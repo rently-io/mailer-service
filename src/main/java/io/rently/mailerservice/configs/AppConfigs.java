b157047c2fbb4b576175c744d04941b2262a428e
chandle greff <unconfigured@null.spigotmc.org>
2022-05-19 11:25:58 +0200

    Removed service constructor

--- src/main/java/io/rently/mailerservice/configs/AppConfigs.java
+++ src/main/java/io/rently/mailerservice/configs/AppConfigs.java
@@ -1,9 +1,12 @@
 package io.rently.mailerservice.configs;
 
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.rently.mailerservice.interfaces.IMailer;
+import io.rently.mailerservice.mailer.Mailer;
 import io.rently.mailerservice.middlewares.Interceptor;
 import io.rently.mailerservice.utils.Broadcaster;
 import io.rently.mailerservice.utils.Jwt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class AppConfigs implements WebMvcConfigurer {
@@ -35,5 +40,16 @@ public class AppConfigs implements WebMvcConfigurer {
         Broadcaster.info("Loaded service middleware with secret `" + secret + "`");
         Broadcaster.info("Loaded service middleware with algo `" + algo + "`");
         return new Jwt(secret, algo);
+    }
+
+    @Bean
+    public IMailer mailer(
+            @Value("${mailer.password}") String password,
+            @Value("${mailer.email}") String email,
+            @Value("${mailer.host}") String host
+    ) {
+        Broadcaster.info("Default mailer loaded with host " + host);
+        Broadcaster.info("Default mailer loaded responding with email " + email);
+        return new Mailer.Builder(email).credentials(email, password).host(host).build();
     }
 }
