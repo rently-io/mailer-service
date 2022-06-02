e72052b4c121b9566b3a55e95e0ac81aef88e810
chandle greff <unconfigured@null.spigotmc.org>
2022-06-02 12:53:29 +0200

    Added helper

--- src/main/java/io/rently/mailerservice/services/ReporterService.java
+++ src/main/java/io/rently/mailerservice/services/ReporterService.java
@@ -1,8 +1,8 @@
 package io.rently.mailerservice.services;
 
 import io.rently.mailerservice.interfaces.IMailer;
-import io.rently.mailerservice.mailer.Mailer;
 import io.rently.mailerservice.mailer.templates.DevError;
+import io.rently.mailerservice.mailer.templates.interfaces.ITemplate;
 import io.rently.mailerservice.utils.Broadcaster;
 import io.rently.mailerservice.utils.Fields;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +16,10 @@ import java.util.Map;
 @Service
 public class ReporterService {
 
-    @Value("#{'${first.responders}'.split(',')}")
-    public List<String> firstResponders;
     @Autowired
     private IMailer mailer;
+    @Value("#{'${first.responders}'.split(',')}")
+    public List<String> firstResponders;
 
     public void sendReportToDevs(Map<String, Object> data) {
         String service = Fields.tryGetOptional("service", data, "Unknown source");
@@ -28,15 +28,17 @@ public class ReporterService {
         String trace = Fields.tryGetOptional("trace", data, "No trace");
         String exceptionType = Fields.tryGetOptional("exceptionType", data, "Unknown");
         DevError devError = new DevError(service, message, cause, trace, exceptionType, firstResponders, new Date().toString());
-
         Broadcaster.info("Dispatching error report from " + service + " to " + firstResponders.size() + " dev(s)");
-
         for (String email : firstResponders) {
-            try {
-                mailer.sendEmail(email, "[ERROR] " + service, devError.getTemplate());
-            } catch(Exception ex) {
-                Broadcaster.warn("Could not notify email: " + email);
-            }
+            trySendingEmail(email, "[ERROR] " + service, devError);
+        }
+    }
+
+    public void trySendingEmail(String email, String subject, ITemplate template) {
+        try {
+            mailer.sendEmail(email, subject, template.getTemplate());
+        } catch(Exception ex) {
+            Broadcaster.warn("Could not notify email: " + email);
         }
     }
 }
