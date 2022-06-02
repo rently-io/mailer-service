47bb70b49b244840f7b270be1f64b13397c1aae8
chandle greff <unconfigured@null.spigotmc.org>
2022-06-02 11:31:30 +0200

    Added dev error test

--- src/test/java/io/rently/mailerservice/mailer/templates/DevErrorTest.java
+++ src/test/java/io/rently/mailerservice/mailer/templates/DevErrorTest.java
@@ -2,11 +2,31 @@ package io.rently.mailerservice.mailer.templates;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DevErrorTest {
 
     @Test
-    void getTemplate() {
+    void getTemplate_validHtml() {
+        String service = "service";
+        String message = "message";
+        String cause = "cause";
+        String trace = "trace";
+        String exceptionType = "exception";
+        List<String> emails = List.of("email1", "email2");
+        String time = "time";
+
+        DevError devError = new DevError(service, message, cause, trace, exceptionType, emails, time);
+
+        String template = devError.getTemplate();
+
+        assert template.contains(service);
+        assert template.contains(message);
+        assert template.contains(cause);
+        assert template.contains(trace);
+        assert template.contains(exceptionType);
+        assert template.contains(time);
     }
 }
