0a03fa4d5eeb4ffee09180c16e0b654863a15077
chandle greff <unconfigured@null.spigotmc.org>
2022-06-02 12:14:57 +0200

    Added trySendEmail method

--- src/main/java/io/rently/mailerservice/services/MailerService.java
+++ src/main/java/io/rently/mailerservice/services/MailerService.java
@@ -4,6 +4,7 @@ import io.rently.mailerservice.errors.Errors;
 import io.rently.mailerservice.interfaces.IMailer;
 import io.rently.mailerservice.mailer.Mailer;
 import io.rently.mailerservice.mailer.templates.*;
+import io.rently.mailerservice.mailer.templates.interfaces.ITemplate;
 import io.rently.mailerservice.utils.Broadcaster;
 import io.rently.mailerservice.utils.Fields;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,28 +24,16 @@ public class MailerService {
         String body = Fields.tryGet("body", data);
         String email = Fields.tryGet("email", data);
         Notification notification = new Notification(subject, body);
-
         Broadcaster.info("Sending generic notification to " + email);
-
-        try {
-            mailer.sendEmail(email, subject, notification.getTemplate());
-        } catch(Exception ex) {
-            throw Errors.INVALID_EMAIL_ADDRESS;
-        }
+        trySendEmail(email, subject, notification);
     }
 
     public void sendGreetings(Map<String, Object> data) {
         String name = Fields.tryGet("name", data);
         String email = Fields.tryGet("email", data);
         Welcome welcome = new Welcome(name);
-
         Broadcaster.info("Sending greetings to " + email);
-
-        try {
-            mailer.sendEmail(email, "Nice to meet you, " + name, welcome.getTemplate());
-        } catch(Exception ex) {
-            throw Errors.INVALID_EMAIL_ADDRESS;
-        }
+        trySendEmail(email, "Nice to meet you, " + name, welcome);
     }
 
     public void sendNewListingNotification(Map<String, Object> data) {
@@ -52,30 +41,18 @@ public class MailerService {
         String link = Fields.tryGet("link", data);
         String image = Fields.tryGet("image", data);
         String title = Fields.tryGet("title", data);
-        String description = Fields.tryGetElipsed("description", data, 100);
-        NewListing newListing = new NewListing(link, image, title, description);
-
         Broadcaster.info("Sending new listing prompt to " + email);
-
-        try {
-            mailer.sendEmail(email, "Listing online!", newListing.getTemplate());
-        } catch(Exception ex) {
-            throw Errors.INVALID_EMAIL_ADDRESS;
-        }
+        String description = Fields.tryGetElipsed("description", data, 100);
+        NewListing newListing = new NewListing(title, image, description, link);
+        trySendEmail(email, "Listing online!", newListing);
     }
 
     public void sendAccountDeletionNotification(Map<String, Object> data) {
         String email = Fields.tryGet("email", data);
         String name = Fields.tryGet("name", data);
         Goodbye goodbyes = new Goodbye(name);
-
         Broadcaster.info("Sending goodbyes to " + email);
-
-        try {
-            mailer.sendEmail(email, "Account remove from Rently", goodbyes.getTemplate());
-        } catch(Exception ex) {
-            throw Errors.INVALID_EMAIL_ADDRESS;
-        }
+        trySendEmail(email, "Account remove from Rently", goodbyes);
     }
 
     public void sendListingDeletionNotification(Map<String, Object> data) {
@@ -83,14 +60,8 @@ public class MailerService {
         String title = Fields.tryGet("title", data);
         String description = Fields.tryGetElipsed("description", data, 100);
         ListingDeletion listingDeletion = new ListingDeletion(title, description);
-
         Broadcaster.info("Sending listing deletion prompt to " + email);
-
-        try {
-            mailer.sendEmail(email, "Listing removed", listingDeletion.getTemplate());
-        } catch(Exception ex) {
-            throw Errors.INVALID_EMAIL_ADDRESS;
-        }
+        trySendEmail(email, "Listing removed!", listingDeletion);
     }
 
     public void sendUpdateListingNotification(Map<String, Object> data) {
@@ -100,11 +71,13 @@ public class MailerService {
         String title = Fields.tryGet("title", data);
         String description = Fields.tryGetElipsed("description", data, 100);
         UpdatedListing updatedListing = new UpdatedListing(link, image, title, description);
-
         Broadcaster.info("Sending updated listing prompt to " + email);
+        trySendEmail(email, "Listing updated!", updatedListing);
+    }
 
+    public void trySendEmail(String email, String subject, ITemplate template) {
         try {
-            mailer.sendEmail(email, "Listing updated!", updatedListing.getTemplate());
+            mailer.sendEmail(email, subject, template.getTemplate());
         } catch(Exception ex) {
             throw Errors.INVALID_EMAIL_ADDRESS;
         }
