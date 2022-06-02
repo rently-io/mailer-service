c4b31d3b7bc9f8fd739c67f853ee5527d4c76707
chandle greff <unconfigured@null.spigotmc.org>
2022-06-02 13:13:24 +0200

    Added ReporterServiceTest

---
+++ src/test/java/io/rently/mailerservice/configs/ReporterServiceTestConfigs.java
@@ -0,0 +1,18 @@
+package io.rently.mailerservice.configs;
+
+import io.rently.mailerservice.interfaces.IMailer;
+import io.rently.mailerservice.services.ReporterService;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+@TestConfiguration
+public class ReporterServiceTestConfigs {
+
+    @Bean
+    @Primary
+    public IMailer iMailer() {
+        return Mockito.mock(IMailer.class);
+    }
+}
--- src/test/java/io/rently/mailerservice/services/ReporterServiceTest.java
+++ src/test/java/io/rently/mailerservice/services/ReporterServiceTest.java
@@ -1,13 +1,112 @@
 package io.rently.mailerservice.services;
 
+import io.rently.mailerservice.configs.BugsnagTestConfigs;
+import io.rently.mailerservice.configs.MailerServiceTestConfigs;
+import io.rently.mailerservice.configs.ReporterServiceTestConfigs;
+import io.rently.mailerservice.errors.Errors;
+import io.rently.mailerservice.interfaces.IMailer;
+import io.rently.mailerservice.mailer.templates.DevError;
+import io.rently.mailerservice.mailer.templates.Notification;
+import io.rently.mailerservice.utils.Broadcaster;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+@ContextConfiguration(classes = { ReporterServiceTestConfigs.class, BugsnagTestConfigs.class })
+@WebMvcTest(ReporterService.class)
 class ReporterServiceTest {
 
-    @Test
-    void sendReportToDevs_mailerInvoked_void() {
+    @Autowired
+    public ReporterService service;
+    @Autowired
+    public IMailer mailer;
 
+    @Test
+    void sendReportToDevs_mailerInvoked_void() throws Exception {
+        String serviceName = "Mailer service";
+        Exception exception = new Exception("This is an exception");
+        Map<String, Object> report = new HashMap<>();
+        report.put("datetime", new Date());
+        report.put("message", exception.getMessage());
+        report.put("service", serviceName);
+        report.put("cause", "cause");
+        report.put("trace", Arrays.toString(exception.getStackTrace()));
+        report.put("exceptionType", exception.getClass());
+        DevError devError = new DevError(
+                serviceName,
+                exception.getMessage(),
+                "cause",
+                Arrays.toString(exception.getStackTrace()),
+                exception.getClass().toString(),
+                service.firstResponders,
+                new Date().toString()
+        );
+        String subject = "[ERROR] " + serviceName;
+
+        service.sendReportToDevs(report);
+
+        verify(mailer, times(service.firstResponders.size())).sendEmail(
+                argThat(email_ -> {
+                    assert service.firstResponders.contains(email_);
+                    return true;
+                }),
+                argThat(subject_ -> {
+                    assert Objects.equals(subject_, subject);
+                    return true;
+                }),
+                argThat(content_ -> {
+                    Broadcaster.debug(content_);
+                    Broadcaster.debug(devError.getTemplate());
+                    assert Objects.equals(content_, devError.getTemplate());
+                    return true;
+                })
+        );
+    }
+
+    @Test
+    void trySendEmail_mailerInvoked_void() throws Exception {
+        String email = "email";
+        String subject = "subject";
+        String body = "body";
+        Notification template = new Notification(subject, body);
+
+        service.trySendingEmail(email, subject, template);
+
+        verify(mailer, times(1)).sendEmail(
+                argThat(email_ -> {
+                    assert Objects.equals(email_, email);
+                    return true;
+                }),
+                argThat(subject_ -> {
+                    assert Objects.equals(subject_, subject);
+                    return true;
+                }),
+                argThat(content_ -> {
+                    assert Objects.equals(content_, template.getTemplate());
+                    return true;
+                })
+        );
+    }
+
+    @Test
+    void trySendEmail_invalidEmail_noExceptionThrown() throws Exception {
+        String email = "email";
+        String subject = "subject";
+        String body = "body";
+        Notification template = new Notification(subject, body);
+
+        Mockito.doThrow(new Exception())
+                .when(mailer).sendEmail(email, subject, template.getTemplate());
+
+        assertDoesNotThrow(() -> service.trySendingEmail(email, subject, template));
     }
 }
