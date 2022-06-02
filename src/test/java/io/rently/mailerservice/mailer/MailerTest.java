d63437e5a48cf17ad226d7ad74b4478634661270
chandle greff <unconfigured@null.spigotmc.org>
2022-06-02 11:40:00 +0200

    Added mailer class tests

--- src/test/java/io/rently/mailerservice/mailer/MailerTest.java
+++ src/test/java/io/rently/mailerservice/mailer/MailerTest.java
@@ -1,12 +1,46 @@
 package io.rently.mailerservice.mailer;
 
+import io.rently.mailerservice.configs.BugsnagTestConfigs;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.mail.MessagingException;
 
+import java.util.Properties;
+
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ContextConfiguration(classes = BugsnagTestConfigs.class)
 class MailerTest {
 
+    public Mailer mailer;
+
+    @BeforeEach
+    public void setup() {
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", 465);
+        properties.put("mail.smtp.ssl.enable", true);
+        properties.put("mail.smtp.auth", true);
+
+        this.mailer = new Mailer(
+                "info.rently.io@gmail.com",
+                "info.rently.io@gmail.com",
+                "ncsyuuohohavmgss",
+                properties);
+    }
+
     @Test
-    void sendEmail() {
+    void sendEmail() throws MessagingException {
+        String recipient = "greffchandler80@gmail.com";
+        String subject = "This is a subject";
+        String content = "This is some content";
+
+        mailer.sendEmail(recipient, subject, content);
     }
 }
