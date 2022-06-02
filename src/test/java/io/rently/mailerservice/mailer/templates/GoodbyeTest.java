7f6d4ccc4358b68311588241a11c2d90881cceaa
chandle greff <unconfigured@null.spigotmc.org>
2022-06-02 11:31:39 +0200

    Added dev error test

--- src/test/java/io/rently/mailerservice/mailer/templates/GoodbyeTest.java
+++ src/test/java/io/rently/mailerservice/mailer/templates/GoodbyeTest.java
@@ -2,11 +2,20 @@ package io.rently.mailerservice.mailer.templates;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GoodbyeTest {
 
     @Test
-    void getTemplate() {
+    void getTemplate_validHtml() {
+        String person = "person";
+
+        Goodbye goodbyes = new Goodbye(person);
+
+        String template = goodbyes.getTemplate();
+
+        assert template.contains(person);
     }
 }
